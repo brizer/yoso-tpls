e3944443ea27d1a1929fd62d418d065e40b9d182
刘放 <362512489@qq.com>
2019-11-14 16:39:15 +0800

    feat: add vue demo

---
+++ vuedemo/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        this is my demo
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    
+}
+</script>
---
+++ vuedemo/webpack.config.js
@@ -0,0 +1,19 @@
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: {
+    bundle1: "./index.js"
+  },
+  output: {
+    filename: "index.js"
+  },
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.vue$/, use: ["vue-loader"] },
+      { test: /\.css$/, use: ["vue-style-loader", "css-loader"] }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
+};
